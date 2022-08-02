4f034aaccdcdb62d9fa2a2555d7a87e8c0968ded
Adil <adilking0071@gmail.com>
2022-08-02 16:26:43 +0500

    Added Search Filter Logic

--- Task/ViewModels/LaunchViewModel.swift
+++ Task/ViewModels/LaunchViewModel.swift
@@ -15,17 +15,40 @@ class LaunchViewModel: ObservableObject {
     
     @Published  var launches =  [Launch]()
     var oldData = [Launch]()
+    
+    
     @Published  var listLoadingError: String = ""
     @Published  var showAlert: Bool = false
+    @Published  var searchTextAndQuery = SearchAndQuery()
     private var subscription: Set<AnyCancellable> = []
     var dataManager: ServiceProtocol
     
     init( dataManager: ServiceProtocol = Services.shared) {
         self.dataManager = dataManager
         getStarLinkList()
-      
+        validate()
     }
-    
+    func validate(){
+        $searchTextAndQuery
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main) // debounces the string publisher, such that it delays the process of sending request to remote server.
+            .removeDuplicates()
+            .map({ (string) -> String? in
+                if string.searchText.count < 1 {
+                    self.launches = self.oldData
+                    return nil
+                }
+                
+                return string.searchText
+            })// prevents sending numerous requests and sends nil if the count of the characters is less than 1.
+            .compactMap{ $0 } // removes the nil values so the search string does not get passed down to the publisher chain
+            .sink { (_) in
+                //
+            } receiveValue: { [self] _ in
+                
+                searchStarLink(query: self.searchTextAndQuery.query ,searchText: self.searchTextAndQuery.searchText)
+            }.store(in: &subscription)
+        
+    }
     
     func getStarLinkList() {
         dataManager.fetchStarLinks()
